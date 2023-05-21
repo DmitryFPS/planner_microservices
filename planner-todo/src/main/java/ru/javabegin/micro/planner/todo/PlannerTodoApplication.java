9d98266b9cf19458ec5188cf21b65de8cd661468
Dima <ddd12@mail.ru>
2023-05-22 00:18:26 +0300

    T6

--- planner-todo/src/main/java/ru/javabegin/micro/planner/todo/PlannerTodoApplication.java
+++ planner-todo/src/main/java/ru/javabegin/micro/planner/todo/PlannerTodoApplication.java
@@ -2,10 +2,10 @@ package ru.javabegin.micro.planner.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class PlannerTodoApplication {
     public static void main(String[] args) {
         SpringApplication.run(PlannerTodoApplication.class, args);
