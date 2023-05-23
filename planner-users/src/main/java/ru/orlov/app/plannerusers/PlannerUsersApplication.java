49f7794fb61783e50dae8b0efb98dad07d93b013
Dima <ddd12@mail.ru>
2023-05-23 11:47:35 +0300

    T16

--- planner-users/src/main/java/ru/orlov/app/plannerusers/PlannerUsersApplication.java
+++ planner-users/src/main/java/ru/orlov/app/plannerusers/PlannerUsersApplication.java
@@ -5,11 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @ComponentScan(basePackages = {"ru.orlov.micro.planner"})
-//@EnableJpaRepositories(basePackages = {"ru.orlov.micro.planner"})
+@EnableJpaRepositories(basePackages = {"ru.orlov.app.plannerusers.dao"})
 @EntityScan(basePackages = {"ru.orlov.micro.planner.entity"})
 public class PlannerUsersApplication {
     public static void main(String[] args) {
